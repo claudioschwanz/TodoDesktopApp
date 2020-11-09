e6246fb68da8ddeb66807d58f588dc54bb5385dc
Claudio Schwanz <claudio.schwanz@otimis.com>
2020-11-09 13:12:34 -0300

    EF Set Up

---
+++ TaskMasterTutorial/Models/DBTASK_MASTERContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TaskMasterTutorial.Models
+{
+    public partial class DBTASK_MASTERContext : DbContext
+    {
+
+        public DBTASK_MASTERContext()
+        {
+        }
+
+        public DBTASK_MASTERContext(DbContextOptions<DBTASK_MASTERContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;database=DBTASK_MASTER;user=root", x => x.ServerVersion("10.4.14-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+        public DbSet<Status> Statuses { get; set; }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
